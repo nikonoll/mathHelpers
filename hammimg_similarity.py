2a583bd3948c87d7523acdf6576e4b0890b91eff
Niko Noll <coutermarsh.mike@gmail.com>
2016-06-27 10:20:37 +0200

    added comments

--- hammimg_similarity.py
+++ hammimg_similarity.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 def distH(x,y):
   "calculates the distance between two data points"
   return sum([abs(xi-yi) for xi, yi in zip(x,y)])
@@ -9,6 +8,7 @@ def simH(y, x):
   "calculates the hamming similarity for 2 points"
   return 1-0.1*distH(x,y)
 
+# TODO: import test data via csv
 cinemaData = np.array([
   (1,0,1,0,1,1,1,0,1,1),
   (0,1,1,0,0,0,1,0,0,0),
@@ -31,8 +31,8 @@ testData = np.array([
   (0,0,0,1,1,1,1,0,0,0)
   ])
 
-
+# ---------------------------
 i = 1
 for d in cinemaData:
   print "x", i, ": ", (simH(d, testData[0]))
-  i = i +1
+  i = i + 1
