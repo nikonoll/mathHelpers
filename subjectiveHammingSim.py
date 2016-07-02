285411d51260f9600a8f123f6822a78cfbf89c95
Niko Noll <dernikonoll@gmail.com>
2016-07-02 11:36:00 +0200

    finalized calculations

--- subjectiveHammingSim.py
+++ subjectiveHammingSim.py
@@ -12,9 +12,12 @@ weights = np.array([
   ('weights', 0.5, 0.4, 0.6, 0.3, 0.7)
   ])
 
+normFactor = (0.5+0.4+0.6+0.3+0.7)
+print(normFactor)
+
 def simpleSim(x,y):
   "calculates sim value on non binary attributes"
-  return 1-(abs(x-y)/x+y)
+  return float(1-(abs(x-y)/(x+y)))
 
 def binarySim(x,y):
   "calculates a similarity value on binary attributes"
@@ -23,10 +26,58 @@ def binarySim(x,y):
   else:
     return 0
 
+def greaterThanBinarySim(x,y):
+  "calculates a similarity value if value greater or equal than threshold"
+  if(x >= y):
+    return 1
+  else:
+    return 0
+
+def smallerThanBinarySim(x,y):
+  "calculates a similarity value if value smaller or equal than threshold"
+  if(x <= y):
+    return 1
+  else:
+    return 0
+
 def subjectiveSim(a, b, w):
   "calculates similarity between two stuntment"
-  test = simpleSim(float(a.item(1)),float(b.item(1)))
-  print(test)
-  # print(simpleSim(a[1], b[1]))
+  "a is the actual stuntman, b the wanted specification"
+  height = simpleSim(float(a.item(1)),float(b.item(1)))*float(w.item(1))
+  weight = simpleSim(float(a.item(2)), float(b.item(2)))*float(w.item(2))
+  specialSim = int(binarySim(a.item(5), b.item(5)))
+  special = float(0)
+  if(specialSim == 1):
+    special = float(w.item(5))
 
-subjectiveSim(stuntmenData[0], stuntmenData[1], weights[0])
+  salary1 = float(a.item(3))
+  salary2 = float(b.item(3))
+  salaryweight = float(w.item(3))
+  salary = float(smallerThanBinarySim(salary1, salary2))*salaryweight
+  ex1 = float(a.item(4))
+  ex2 = float(b.item(4))
+  exWeight = float(w.item(4))
+  experience = float(greaterThanBinarySim(ex1, ex2))*exWeight
+  results = [height, weight, salary, experience,special]
+  return (results)
+
+t1wantedMan = np.array([
+  ('t1wanted', 180, 85, 4000, 4, 'feuer')
+  ])
+
+t2wantedMan = np.array([
+  ('t2wanted', 175, 80, 3000, 5, 'springen')
+  ])
+
+print("similarities for T1: ")
+print("-----------data-----------")
+print(t1wantedMan[0])
+for man in stuntmenData:
+  print(man)
+
+print("---------similarites----------")
+for man in stuntmenData:
+  x = subjectiveSim(man, t1wantedMan[0], weights[0])
+  print(man.item(0), ":  ")
+  print((sum(x)/normFactor))
+  print(x)
